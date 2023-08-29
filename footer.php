c5822e943359cf8155c636569f96f420c80c1666
Nuhu wambali <100964150+NuhuWambali@users.noreply.github.com>
2023-08-29 09:46:47 +0300

    Initial commit

---
+++ footer.php
@@ -0,0 +1,94 @@
+
+<footer class="footer-wrapper">
+			
+            <div class="main-footer">
+            
+                <div class="container">
+                
+                    <div class="row">
+                    
+                        <div class="col-sm-12 col-md-9">
+                        
+                            <div class="row">
+                            
+                                <div class="col-sm-6 col-md-4">
+                                
+                                    <div class="footer-about-us">
+                                        <h5 class="footer-title">About fastJobs</h5>
+                                        <p  style="color:#fff" >fastJobs, is an online job management system developed by Nuhu Wambali for his project in may 2023.</p>
+                                    
+                                    </div>
+
+                                </div>
+                                
+                                <div class="col-sm-6 col-md-5 mt-30-xs">
+                                    <h5 class="footer-title">Quick Links</h5>
+                                    <ul class="footer-menu clearfix">
+                                        <li><a href="./">Home</a></li>
+                                        <li><a href="job-list.php">Job List</a></li>
+                                        <li><a href="employers.php">Employers</a></li>
+                                        <li><a href="employees.php">Employees</a></li>
+                                        <li><a href="contact.php">Contact Us</a></li>
+                                        <li><a href="#">Go to top</a></li>
+
+                                    </ul>
+                                
+                                </div>
+
+                            </div>
+
+                        </div>
+                        
+                        <div class="col-sm-12 col-md-3 mt-30-sm">
+                        
+                            <h5 class="footer-title">fastJobs Contact</h5>
+                            
+                            <p  style="color:#fff" >Address : Kampala International University, Dar es salaam</p>
+                            <p  style="color:#fff" >Email : <a href="mailto:fastjobs.nuhu@gmail.com">fastjobs.nuhu@gmail.com</a></p>
+                            <p  style="color:#fff" >Phone : <a href="tel:+2556131727">+255 613 172 799</a></p>
+                            
+
+                        </div>
+
+                        
+                    </div>
+                    
+                </div>
+                
+            </div>
+            <hr class="hr2">
+            <div class="bottom-footer">
+            
+                <div class="container">
+                
+                    <div class="row">
+                    
+                        <div class="col-sm-4 col-md-4">
+                
+                            <p style="color:#fff" class="copy-right">&#169; Copyright <?php echo date('Y'); ?> fastJob Software</p>
+                            
+                        </div>
+                        
+                        <div class="col-sm-4 col-md-4">
+                        
+                            <ul class="bottom-footer-menu">
+                                <li><a >Developed by Nuhu Wambali</a></li>
+                            </ul>
+                        
+                        </div>
+                        
+                        <div class="col-sm-4 col-md-4">
+                            <ul class="bottom-footer-menu for-social">
+                                <li><a href="<?php echo "$tw"; ?>"><i class="ri ri-twitter" data-toggle="tooltip" data-placement="top" title="twitter"></i></a></li>
+                                <li><a href="<?php echo "$fb"; ?>"><i class="ri ri-facebook" data-toggle="tooltip" data-placement="top" title="facebook"></i></a></li>
+                                <li><a href="<?php echo "$ig"; ?>"><i class="ri ri-instagram" data-toggle="tooltip" data-placement="top" title="instagram"></i></a></li>
+                            </ul>
+                        </div>
+                    
+                    </div>
+
+                </div>
+                
+            </div>
+        
+        </footer>
